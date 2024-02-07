5b468f930c153391296b47a208564f6827b5a865
AyelDev <aarielabelgas23@gmail.com>
2024-02-07 20:43:57 +0800

    Adding dependencies and login page

--- StudyWizard/StudyWizard/StudyWizard/App.xaml.cs
+++ StudyWizard/StudyWizard/StudyWizard/App.xaml.cs
@@ -10,7 +10,7 @@ namespace StudyWizard
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new Login());
         }
 
         protected override void OnStart()
